0a540451ed423631db5a0fb3a8d46fe838327f76
David Rio Deiros <driodeiros@gmail.com>
2015-01-13 11:44:54 -0500

    from libs to lib

--- webapp-skelethon/bootstrap/js/app.js
+++ webapp-skelethon/bootstrap/js/app.js
@@ -1,1 +1,14 @@
 /* vim: set ts=4 sw=4 et: */
+
+(function() {
+
+var data = {};
+
+function loadData() {
+    d3.tsv("data/files.txt")
+      .get(function(error, rows) { console.log(rows); });
+}
+
+loadData();
+
+})();
