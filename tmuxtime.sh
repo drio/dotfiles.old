b0338ffd71679a3c99ba9909d44202db75adafda
David Rio Deiros <driodeiros@gmail.com>
2015-01-17 17:41:36 -0500

    Separate logic

--- tmuxtime.sh
+++ tmuxtime.sh
@@ -2,17 +2,7 @@
 #
 SESSION=0
 
-if [ $1 == "setup" ]
-then
-  #  vim
-  vim_window=$(tmux list-window -t $SESSION: | grep vim | awk -F: '{print $1}')
-  if [ ".$vim_window" == "." ];then
-    echo "+ Creating vim window ..."
-    tmux new-window -n vim -t $SESSION:
-    tmux send-keys -t $SESSION:$ardmore 'vim' C-m
-  fi
-
-  # ardmore
+ardmore() {
   ard_window=$(tmux list-window -t $SESSION: | grep ardmore | awk -F: '{print $1}')
   if [ ".$ard_window" != "." ];then
     echo "+ Killing ardmore window ..."
@@ -21,8 +11,20 @@ then
   tmux new-window -n ardmore -t $SESSION:
   ard_window=$(tmux list-window -t $SESSION: | grep ardmore | awk -F: '{print $1}')
   tmux send-keys -t $SESSION:$ardmore 'vpn' C-m
+}
 
-  # mutt/irssi
+
+vim() {
+  vim_window=$(tmux list-window -t $SESSION: | grep vim | awk -F: '{print $1}')
+  if [ ".$vim_window" == "." ];then
+    echo "+ Creating vim window ..."
+    tmux new-window -n vim -t $SESSION:
+    tmux send-keys -t $SESSION:$ardmore 'vim' C-m
+  fi
+}
+
+
+mutt_irssi() {
   irssi_window=$(tmux list-window -t $SESSION: | grep irssi | awk -F: '{print $1}')
   if [ ".$irssi_window" == "." ];then
     echo "+ Creating irssi window..."
@@ -33,31 +35,42 @@ then
     tmux send-keys -t $SESSION:$irssi_window.1 'ssh -t is tmux a' C-m
     tmux split-window -v -t $SESSION:$irssi_window.1
   fi
+}
+
+
+pis() {
+  pis_window=$(tmux list-window -t $SESSION: | grep pis | awk -F: '{print $1}')
+  if [ ".$pis_window" != "." ];then
+    tmux kill-window -t $SESSION:$pis_window
+  fi
+  echo "+ Creating pis window..."
+  tmux new-window -n pis -t $SESSION:
+  pis_window=$(tmux list-window -t $SESSION: | grep pis | awk -F: '{print $1}')
+  tmux split-window -h -t $SESSION:$pis_window
+  tmux send-keys -t $SESSION:$pis_window.0 'ssh -t pi@192.168.1.109 tmux a' C-m
+  tmux send-keys -t $SESSION:$pis_window.1 'ssh -t  -c aes256-ctr pi@192.168.1.132  tmux a' C-m
+  # Go to the vim window
+  vim_window=$(tmux list-window -t $SESSION: | grep vim | awk -F: '{print $1}')
+  tmux select-window -t $SESSION:$vim_window
+}
+
+
+setup() {
+  vim
+  ardmore
+  mutt_irssi
+  pis
 
   # Go to the vim window
   vim_window=$(tmux list-window -t $SESSION: | grep vim | awk -F: '{print $1}')
   tmux select-window -t $SESSION:$vim_window
-fi
+}
 
-if [ $1 == "split" ]
-then
+
+split() {
   vim_window=$(tmux list-window -t $SESSION: | grep vim | awk -F: '{print $1}')
   tmux split-window -t $SESSION:$vim_window
   tmux resize-pane -t $SESSION:$vim_window.1 -y 10
-fi
+}
 
-if [ $1 == "pis" ]
-then
-  pis_window=$(tmux list-window -t $SESSION: | grep pis | awk -F: '{print $1}')
-  if [ ".$irssi_window" == "." ];then
-    echo "+ Creating pis window..."
-    tmux new-window -n pis -t $SESSION:
-    pis_window=$(tmux list-window -t $SESSION: | grep pis | awk -F: '{print $1}')
-    tmux split-window -h -t $SESSION:$pis_window
-    tmux send-keys -t $SESSION:$pis_window.0 'ssh -t pi@192.168.1.109 tmux a' C-m
-    tmux send-keys -t $SESSION:$pis_window.1 'ssh -t  -c aes256-ctr pi@192.168.1.132  tmux a' C-m
-    # Go to the vim window
-    vim_window=$(tmux list-window -t $SESSION: | grep vim | awk -F: '{print $1}')
-    tmux select-window -t $SESSION:$vim_window
-  fi
-fi
+$1
