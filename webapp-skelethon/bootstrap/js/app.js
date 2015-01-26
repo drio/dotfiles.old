dbade1efbd95d311ff1ba2e8882e87a9e6522380
David Rio Deiros <driodeiros@gmail.com>
2015-01-26 16:18:10 -0700

    Adding jquery

--- webapp-skelethon/bootstrap/js/app.js
+++ webapp-skelethon/bootstrap/js/app.js
@@ -1,14 +1,1 @@
 /* vim: set ts=4 sw=4 et: */
-
-(function() {
-
-var data = {};
-
-function loadData() {
-    d3.tsv("data/files.txt")
-      .get(function(error, rows) { console.log(rows); });
-}
-
-loadData();
-
-})();
